703f4964714e001c9389c95be516257ad4542c86
BashirAliFTW <82927455+BashirAliFTW@users.noreply.github.com>
2021-08-31 16:34:27 +0100

    echo test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -55,7 +55,13 @@
 
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                    Laravel has wonderful, thorough documentation covering every aspect of the framework. Whether you are new to the framework or have previous experience with Laravel, we recommend reading all of the documentation from beginning to end.
+                                    Laravel has wonderful, 
+                                    <?php
+                                    $results = DB::select('select * from users where id = ?', array(1));
+                                    echo $results;
+                                    ?>
+                                    
+                                    thorough documentation covering every aspect of the framework. Whether you are new to the framework or have previous experience with Laravel, we recommend reading all of the documentation from beginning to end.
                                 </div>
                             </div>
                         </div>
