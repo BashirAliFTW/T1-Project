044bc5a3fe7d3fae1bdab6667f444a12f391920d
Umar040 <Umar04@hotmail.co.uk>
2021-09-01 10:31:50 +0100

    renamed row to nav for navigation bar in index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -29,7 +29,7 @@
         @endif
     </div>
     <!--Navigation Bar-->
-    <div class="row">
+    <div class="nav">
         <div class="col" align="center" style="background-color:lavender;"><a href="signin.html">Sign Out</a>
         </div>
         <div class="col" align="center" style="background-color:lavender;"><a href="addanarticle.html">Add an
