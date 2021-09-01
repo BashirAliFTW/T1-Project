223a8589e00d2e4489ae163d9e6e590ec1717b6e
blackmamba4 <89778237+blackmamba4@users.noreply.github.com>
2021-09-01 13:13:48 +0100

    bug

--- resources/views/addarticle.blade.php
+++ resources/views/addarticle.blade.php
@@ -33,7 +33,7 @@
                     
                                         <div class="form-group ">
                                             <label for="inputState" class="text-primary" >Category</label>
-                                            {{ Form::select('category', ['Computers', 'Mobile' , 'Hardware' ]); 
+                                            {{ Form::select('category', ['Computers', 'Mobile' , 'Hardware' ]) }}
                                             </select>
                                         </div>
                                     <div class="form-group">
