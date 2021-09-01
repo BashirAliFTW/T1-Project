56a584dbaa461df80ffb01d20b037867c0d4983d
BashirAliFTW <82927455+BashirAliFTW@users.noreply.github.com>
2021-09-01 13:46:17 +0100

    Update articles.blade.php

--- resources/views/articles.blade.php
+++ resources/views/articles.blade.php
@@ -55,22 +55,16 @@
                         </div>
                         <div class="col-xs-12 col-sm-4">
                             <div class="card">
-                                <a class="img-card" href="http://www.fostrap.com/2016/03/5-button-hover-animation-effects-css3.html">
-                                <img src="https://4.bp.blogspot.com/-TDIJ17DfCco/Vtneyc-0t4I/AAAAAAAABmk/aa4AjmCvRck/s1600/cover.jpg" />
-                              </a>
+                                <a class="img-card" href="http://www.fostrap.com/2016/03/bootstrap-3-carousel-fade-effect.html">
+                                    <img src="https://1.bp.blogspot.com/-Bii3S69BdjQ/VtdOpIi4aoI/AAAAAAAABlk/F0z23Yr59f0/s640/cover.jpg" />
+                                  </a>
                                 <div class="card-content">
                                     <h4 class="card-title">
                                         <a href="http://www.fostrap.com/2016/03/5-button-hover-animation-effects-css3.html">5  Button Hover Animation Effects
                                       </a>
                                     </h4>
                                     <p class="">
-                                        <?php
-                                        $articles = DB::table('users')->cursor();
-                                        foreach ($articles as $data) {
-                                            echo $data->name;
-                                            echo "<br>";
-                                        }
-                                        ?>
+                                        Loren impsum
                                     </p>
                                 </div>
                                 <div class="card-read-more">
