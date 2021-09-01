890c81f2f910f4d245f9aae315fc899b242479d9
blackmamba4 <89778237+blackmamba4@users.noreply.github.com>
2021-09-01 13:58:53 +0100

    redefined migration table

--- database/migrations/2021_09_01_122217_create_articles_table.php
+++ database/migrations/2021_09_01_122217_create_articles_table.php
@@ -16,7 +16,7 @@ class CreateArticlesTable extends Migration
         Schema::create('articles', function (Blueprint $table) {
             $table->id('articleid');
             $table->string('articlesAuthor');
-            $table->string('articlesName');
+            $table->string('articlesTitle');
             $table->date('articlesDate');
             $table->time('articlesTime');
             $table->string('articlesEmail');
