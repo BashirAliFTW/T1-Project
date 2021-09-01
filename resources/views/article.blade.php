5aad9a4ce10575bfff3288f10e52fe4ba7bf8301
blackmamba4 <89778237+blackmamba4@users.noreply.github.com>
2021-09-02 00:04:04 +0100

    layout changes

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -2,9 +2,9 @@
 
 @section('content')
 <div class="row justify-content-center bg-grey">
-    <div class="article-post">
+    <div class="article-post col-md-8">
         <h2>{{$Article->articlesTitle}}</h2>
-        <p class="text-muted"> {{$Article->articlesDate }} by <p class="text-primary">{{$Article->articlesAuthor}}</p></p>
+        <p class="text-muted"> {{$Article->articlesDate }} by <a href="#" class="text-primary">{{$Article->articlesAuthor}}</a></p>
         <p>{{$Article->articlesContent}}</p>
     </div>
 </div>
