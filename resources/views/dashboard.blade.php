ca478f0a94ab15b3db771204215d05a2cb8691d0
blackmamba4 <89778237+blackmamba4@users.noreply.github.com>
2021-09-01 11:50:40 +0100

    rollback

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,6 +1,3 @@
-@extends('layouts.app')
-
-@section('content')
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
@@ -8,36 +5,47 @@
         </h2>
     </x-slot>
 
-    <body>
- 
-
-    <!--Navigation Bar-->
-    <div class="nav">
-        <div class="col" align="center" style="background-color:lavender;"><a href="signin.html">Sign Out</a>
-        </div>
-        <div class="col" align="center" style="background-color:lavender;"><a href="{{ route('article') }}">Add an
-                article</a></div>
-        <div class="col" align="center" style="background-color:lavender;"><a href="aboutus.html">About us</a>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                <div class="container mt-2">
+                    <div class="row">
+                      <div class="col-md-3 col-sm-6">
+                        <div class="card card-block">
+                        <h4 class="card-title text-right"><i class="material-icons">settings</i></h4>
+                      <img src="https://static.pexels.com/photos/7096/people-woman-coffee-meeting.jpg" alt="Photo of sunset">
+                          <h5 class="card-title mt-3 mb-3">Sierra Web Development • Owner</h5>
+                          <p class="card-text">This is a company that builds websites, web apps and e-commerce solutions.</p> 
+                    </div>
+                      </div>
+                      <div class="col-md-3 col-sm-6">
+                        <div class="card card-block">
+                        <h4 class="card-title text-right"><i class="material-icons">settings</i></h4>
+                      <img src="https://static.pexels.com/photos/7357/startup-photos.jpg" alt="Photo of sunset">
+                          <h5 class="card-title  mt-3 mb-3">ProVyuh</h5>
+                          <p class="card-text">This is a company that builds websites, web .</p> 
+                    </div>
+                      </div>
+                      <div class="col-md-3 col-sm-6">
+                        <div class="card card-block">
+                        <h4 class="card-title text-right"><i class="material-icons">settings</i></h4>
+                      <img src="https://static.pexels.com/photos/262550/pexels-photo-262550.jpeg" alt="Photo of sunset">
+                          <h5 class="card-title  mt-3 mb-3">ProVyuh</h5>
+                          <p class="card-text">This is a company that builds websites, web apps and e-commerce solutions.</p> 
+                    </div>
+                      </div>
+                      <div class="col-md-3 col-sm-6">
+                        <div class="card card-block">
+                        <h4 class="card-title text-right"><i class="material-icons">settings</i></h4>
+                      <img src="https://static.pexels.com/photos/326424/pexels-photo-326424.jpeg" alt="Photo of sunset">
+                          <h5 class="card-title  mt-3 mb-3">ProVyuh</h5>
+                          <p class="card-text">This is a company that builds websites, web apps and e-commerce solutions.</p> 
+                    </div>
+                      </div>    
+                    </div>
+                  </div>
+                <x-jet-welcome />
+            </div>
         </div>
     </div>
-    <!--Main Title-->
-    <div class="container.fluid p-3 my-3 bg-dark text-white">
-        <h1 class="display-2" align="center">Techno Times Main Page</h1><br>
-        <h1 class="display-4" align="center">By Team R1</h1>
-    </div>
-    <div class="row">
-        <div class="col">
-            <article id="0">
-                <h1 class="display-5" align="center">Article 1</h1>
-                <p align="center">Article 1 text</p>
-            </article>
-        </div>
-        <div class="col">
-            <article id="1">
-                <h1 class="display-5" align="center">Article 2</h1>
-                <p align="center">Article 2 text</p>
-            </article>
-        </div>
-
-    </div>
-@endsection
+</x-app-layout>
