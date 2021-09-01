7693e324544ead800df0acf92eb1de73629e2032
blackmamba4 <89778237+blackmamba4@users.noreply.github.com>
2021-09-01 23:57:36 +0100

    adding article to article page

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -1,6 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-
+<div class="row justify-content-center bg-grey">
+    <div class="article-post">
+        <h2>{{$Article->articlesTitle}}</h2>
+        <p class="text-muted"> {{$Article->articlesDate }} by <p class="text-primary">{{$Article->articlesAuthor}}</p></p>
+        <p>{{$Article->articlesContent}}</p>
+    </div>
+</div>
 @endsection
 
