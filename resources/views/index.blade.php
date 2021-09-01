4bdbbe9906ba22708ba6e4236b486faac0fe9132
blackmamba4 <89778237+blackmamba4@users.noreply.github.com>
2021-09-01 11:00:15 +0100

    bug in navigation bar causing it to crash when not signed in

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,16 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.app')
 
-<head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Welcome Page</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-</head>
-
+@section('content')
 <body>
     <div
         class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
@@ -57,6 +47,4 @@
         </div>
 
     </div>
-</body>
-
-</html>
+@end section
