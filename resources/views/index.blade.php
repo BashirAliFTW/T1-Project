4259630e1751580d7eccfa1d80343541277515a4
blackmamba4 <89778237+blackmamba4@users.noreply.github.com>
2021-09-01 11:33:39 +0100

    new homepage

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,34 +1,141 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-@section('content')
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        <!-- CSS only -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+
+        <!-- Styles -->
+        <style>
+
+html,
+body {
+  height: 100%;
+  background-color: #fff;
+}
+body {
+  color: #fff;
+  text-align: center;
+  text-shadow: 0 1px 3px rgba(0,0,0,.5);
+}
+
+/* Extra markup and styles for table-esque vertical and horizontal centering */
+.site-wrapper {
+  display: table;
+  width: 100%;
+  height: 600px;
+    background: url("https://news.microsoft.com/wp-content/uploads/2016/07/1L3B3932-copy.jpg") no-repeat center center fixed; 
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+  min-height: 100%;
+}
+.site-wrapper-inner {
+  display: table-cell;
+  vertical-align: top;
+}
+.cover-container {
+  margin-right: auto;
+  margin-left: auto;
+}
+
+/* Padding for spacing */
+.inner {
+  padding: 30px;
+}
+
+/*
+ * Affix and center
+ */
+  /* Start the vertical centering */
+  .site-wrapper-inner {
+    vertical-align: middle;
+    padding-bottom:10%;
+  }
+
+}
+        </style>
+
+        <style>
+            body {
+                font-family: sans-serif;
+            }
+        </style>
+    </head>
+    <body class="antialiased">
+               <nav class="navbar navbar-expand-md navbar-dark bg-primary shadow-sm">
+            <div class="container">
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    TechnoTimes
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                   
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav mr-auto">
+                      
+                       
+                            @if (Route::has('login'))
+                            @auth
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ url('/home') }}">Home</a>
+                                </li>
+                            @else
+                            
+                           
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('login') }}">Login</a>
+                                </li>
+                                @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">Register</a>
+                                </li>
+                                @endif
+                            @endauth
+                            @endif
+                     
+                    </ul>
+                </div>
+            </div>
+           
+        </nav>
     
-    <!--Navigation Bar-->
-    <div class="nav">
-        <div class="col" align="center" style="background-color:lavender;"><a href="signin.html">Sign Out</a>
-        </div>
-        <div class="col" align="center" style="background-color:lavender;"><a href="{{ route('article') }}">Add an
-                article</a></div>
-        <div class="col" align="center" style="background-color:lavender;"><a href="aboutus.html">About us</a>
-        </div>
-    </div>
-    <!--Main Title-->
-    <div class="container.fluid p-3 my-3 bg-dark text-white">
-        <h1 class="display-2" align="center">Techno Times Main Page</h1><br>
-        <h1 class="display-4" align="center">By Team R1</h1>
-    </div>
-    <div class="row">
-        <div class="col">
-            <article id="0">
-                <h1 class="display-5" align="center">Article 1</h1>
-                <p align="center">Article 1 text</p>
-            </article>
-        </div>
-        <div class="col">
-            <article id="1">
-                <h1 class="display-5" align="center">Article 2</h1>
-                <p align="center">Article 2 text</p>
-            </article>
+         <div class="site-wrapper">
+
+             <div class="site-wrapper-inner">
+
+             <div class="container">
+
+          <div class="masthead clearfix">
+           
+             
+          
+            
+          </div>
+
+          <div class="inner cover">
+            <h1 class="cover-heading">TechnoTimes</h1>
+            <p class="lead">For all your daily tech articles</p>
+            
+          </div>
+
         </div>
 
+      </div>
+
     </div>
-@endsection
+           
+     
+    </body>
+</html>
