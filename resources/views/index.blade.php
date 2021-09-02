4daea20a11f5610398137a1209bf333e5264d5da
BashirAliFTW <82927455+BashirAliFTW@users.noreply.github.com>
2021-09-02 01:17:58 +0100

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -24,7 +24,7 @@
                 <div class="container">
                     <div class="row">
                         <?php
-                            $articles = DB::table('articles')->groupBy('articlesTag'); 
+                            $articles = DB::table('articles')->groupBy('articlesTag')->get(); 
                             foreach ($articles as $data)  { ?>
                                 <div class="col-xs-12 col-sm-4">
                                     <div class="card">
